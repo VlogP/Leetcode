b3534951cb3affac14a71a61757ec40f440df1fa
Vladislav <vladislav.panchenko98@gmail.com>
2023-12-03 21:55:37 +0300

    Added _605_Can_Place_Flowers

--- Leetcode/605_Can_Place_Flowers.cs
+++ Leetcode/605_Can_Place_Flowers.cs
@@ -11,8 +11,45 @@ namespace Leetcode
         public bool CanPlaceFlowers(int[] flowerbed, int n)
         {
             var length = flowerbed.Length;
+            var counter = 0;
+            var flowerbed2 = (int[])flowerbed.Clone();
 
-            return true;
-    }
+            if (length > 1)
+            {
+                for (var index = 0; index < length; index++)
+                {
+                    if (index == 0)
+                    {
+                        if (flowerbed2[index + 1] != 1 && flowerbed2[index] != 1)
+                        {
+                            flowerbed2[index] = 1;
+                            counter++;
+                        }
+                    }
+                    else if (index == length - 1)
+                    {
+                        if (flowerbed2[index] != 1 && flowerbed2[index - 1] != 1)
+                        {
+                            flowerbed2[index] = 1;
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        if (flowerbed2[index] != 1 && flowerbed2[index - 1] != 1 && flowerbed2[index + 1] != 1)
+                        {
+                            flowerbed2[index] = 1;
+                            counter++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return n == 0 || flowerbed2[0] == 0;
+            }
+
+            return counter >= n;
+        }
     }
 }
