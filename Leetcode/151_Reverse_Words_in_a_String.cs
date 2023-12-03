37d30befdd5001f6612c8cd038bc5976c247c238
Vladislav <vladislav.panchenko98@gmail.com>
2023-12-04 02:28:25 +0300

    Added 151 238

---
+++ Leetcode/151_Reverse_Words_in_a_String.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class _151_Reverse_Words_in_a_String
+    {
+        public string ReverseWords(string s)
+        {
+            return string.Join(" ", s.Split(" ", StringSplitOptions.RemoveEmptyEntries).Reverse()).Trim();
+        }
+    }
+}
---
+++ Leetcode/238_Product_of_Array_Except_Self.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class _238_Product_of_Array_Except_Self
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var length = nums.Length;
+            var output = new int[length];
+            var multiplier = 1;
+            var index = 0;
+
+            while (index < length)
+            {
+                output[index] = multiplier;
+                multiplier *= nums[index];
+                index++;
+            }
+
+            index = length - 1;
+            multiplier = 1;
+
+            while (index >= 0)
+            {
+                output[index] *= multiplier;
+                multiplier *= nums[index];
+                index--;
+            }
+
+            return output;
+        }
+    }
+}
