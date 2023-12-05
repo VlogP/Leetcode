7e647cd297395c3b215f4430aec4143f4fe8c54b
Vlad <vlad.panchenko98@gmail.com>
2023-12-06 02:21:23 +0300

    Added 14

---
+++ Leetcode/14_Longest_Common_Prefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class _14_Longest_Common_Prefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var minLength = strs.Min(x => x.Length);
+            var longestPrefix = string.Empty;
+            var shortest = strs.FirstOrDefault(x => x.Length == minLength);
+
+            for (var index = 0; index < shortest.Length; index++)
+            {
+                var substring = shortest.Substring(0, index + 1);
+
+                if (strs.All(x => x.StartsWith(substring)))
+                {
+                    longestPrefix = substring;
+                }
+            }
+
+            return longestPrefix;
+        }
+    }
+}
