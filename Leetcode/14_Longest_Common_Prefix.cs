7008981954a7f1573d613831dfd2d75de186b2f8
Vlad <vlad.panchenko98@gmail.com>
2023-12-06 03:09:55 +0300

    Updated 14

--- Leetcode/14_Longest_Common_Prefix.cs
+++ Leetcode/14_Longest_Common_Prefix.cs
@@ -10,21 +10,114 @@ namespace Leetcode
     {
         public string LongestCommonPrefix(string[] strs)
         {
+            if (strs.Count() == 1) return strs[0];
+            if (strs.Count() == 0) return "";
+
             var minLength = strs.Min(x => x.Length);
+            var count = strs.Count();
             var longestPrefix = string.Empty;
-            var shortest = strs.FirstOrDefault(x => x.Length == minLength);
 
-            for (var index = 0; index < shortest.Length; index++)
+            Array.Sort(strs);
+
+            for (var index = 0; index < strs[0].Length; index++)
             {
-                var substring = shortest.Substring(0, index + 1);
-
-                if (strs.All(x => x.StartsWith(substring)))
+                if (strs[0][index] != strs[strs.Length - 1][index])
                 {
-                    longestPrefix = substring;
+                    break;
+                }
+                else
+                {
+                    longestPrefix += strs[0][index];
                 }
             }
 
             return longestPrefix;
         }
+
+
+
+        /*
+                 public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Count() == 1) return strs[0];
+            if (strs.Count() == 0) return "";
+
+            var minLength = strs.Min(x => x.Length);
+            var count = strs.Count();
+            var longestPrefix = string.Empty;
+            var shortestIndex = Array.FindIndex(strs, x => x.Length == minLength);
+
+            for (var index = 0; index < strs[shortestIndex].Length; index++)
+            {
+                var substring = strs[shortestIndex].Substring(0, index + 1);
+
+                for (var index2 = 0; index2 < count; index2++)
+                {
+                    if (index2 != shortestIndex)
+                    {
+                        if (IsPrefix(strs[index2], substring))
+                        {
+                            if (index2 == count - 1)
+                            {
+                                longestPrefix = substring;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (shortestIndex == count - 1)
+                        {
+                            longestPrefix = substring;
+                        }
+                    }
+                }
+            }
+
+            return longestPrefix;
+        }
+
+        public bool IsPrefix(string str, string prefix)
+        {
+            for (var index3 = 0; index3 < prefix.Length; index3++)
+            {
+                if (str[index3] != prefix[index3])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+         */
+
+
+
+
+
+
+        /*
+        public string LongestCommonPrefix(string[] strs) 
+    {
+        var minLength = strs.Min(x => x.Length);
+        var longestPrefix = string.Empty;
+        var shortest = strs.FirstOrDefault(x => x.Length == minLength);
+
+        for(var index = 0; index < shortest.Length; index++)
+        {
+            var substring = shortest.Substring(0, index + 1);
+
+            if (strs.All(x => x.StartsWith(substring)))
+            {
+                longestPrefix = substring;
+            }
+        }
+
+        return longestPrefix;
+    }
+         */
     }
 }
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -6,10 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var c = new _2_Add_Two_Numbers();
+            var c = new _14_Longest_Common_Prefix();
 
-            c.AddTwoNumbers(new _2_Add_Two_Numbers.ListNode(2, new _2_Add_Two_Numbers.ListNode(4, new _2_Add_Two_Numbers.ListNode(9))),
-                new _2_Add_Two_Numbers.ListNode(5, new _2_Add_Two_Numbers.ListNode(6, new _2_Add_Two_Numbers.ListNode(4, new _2_Add_Two_Numbers.ListNode(9)))));
+            c.LongestCommonPrefix(new string[] {"ab", "a"});
         }
     }
 }
