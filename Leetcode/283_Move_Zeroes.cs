9aae8a102e79aa362a2419dbc13e7728f05e482f
Vlad <vlad.panchenko98@gmail.com>
2023-12-18 08:58:43 +0300

    Added 283

---
+++ Leetcode/283_Move_Zeroes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class _283_Move_Zeroes
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            var zeroPointer = 0;
+            var numPointer = 0;
+
+            while (numPointer < nums.Length)
+            {
+                if (nums[numPointer] == 0)
+                {
+                    numPointer++;
+                }
+                else
+                {
+                    var temp = nums[numPointer];
+                    nums[numPointer] = nums[zeroPointer];
+                    nums[zeroPointer] = temp;
+
+                    zeroPointer++;
+                    numPointer++;
+                }
+            }
+        }
+    }
+}
