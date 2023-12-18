4d668858fd4d8d5f964fb22a6044554013d56574
Vlad <vlad.panchenko98@gmail.com>
2023-12-18 12:51:12 +0300

    Added 392

---
+++ Leetcode/392_Is_Subsequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class _392_Is_Subsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            var sPointer = 0;
+            var tPointer = 0;
+
+            while (sPointer < s.Length && tPointer < t.Length)
+            {
+                if (s[sPointer] == t[tPointer])
+                {
+                    sPointer++;
+                    tPointer++;
+                }
+                else
+                {
+                    tPointer++;
+                }
+            }
+
+            return sPointer == s.Length;
+        }
+    }
+}
